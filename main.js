6d72fe4d72f7ba13eb1dac3a3c245c4793b2c41f
Miftahul huda <113413138+M1f74h11@users.noreply.github.com>
2023-09-07 14:13:50 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+alert('Klik OK untuk Melanjutkan')
+let nama =  prompt('masukan nama kamu')
+alert('Hallo ' + nama)
+
+let kabar = prompt('bagaimana kabarnya?')
+alert('alhamdulillah kalau baik, kalau tidak baik semoga kebaikan akan segera datang')
+
+
+
